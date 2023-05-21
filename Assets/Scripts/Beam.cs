8c008084ddfe498d014dbc5375a55d7801d04c3b
EtaOnm256 <manabu4836@hotmail.com>
2023-05-21 09:14:49 +0900

    当たり判定

--- Assets/Scripts/Beam.cs
+++ Assets/Scripts/Beam.cs
@@ -23,15 +23,32 @@ public class Beam : MonoBehaviour
         lineRenderer.SetPosition(1, start_pos);
     }
 
+    RaycastHit[] rayCastHit = new RaycastHit[8];
+
     // Update is called once per frame
     void Update()
     {
+        const float speed = 0.3f;
+
         float length = (lineRenderer.GetPosition(1) - start_pos).magnitude;
 
         length = Mathf.Min(length, MaxLength);
 
-        lineRenderer.SetPosition(1, lineRenderer.GetPosition(1) + direction * 0.3f);
+        Ray ray = new Ray(lineRenderer.GetPosition(1), direction);
+
+        int numhit = Physics.RaycastNonAlloc(ray, rayCastHit,speed,1 << 6);
+
+        for (int i = 0; i < numhit; i++)
+        {
+            GameObject.Destroy(gameObject);
+            return;
+        }
+
+
+        lineRenderer.SetPosition(1, lineRenderer.GetPosition(1) + direction * speed);
 
         lineRenderer.SetPosition(0, lineRenderer.GetPosition(1) - direction * length);
+
+       
     }
 }
