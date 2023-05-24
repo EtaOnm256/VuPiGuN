0c7378673d3295771ccd2330bc0472521e9f8a72
EtaOnm256 <manabu4836@hotmail.com>
2023-05-24 21:36:23 +0900

    ロックオンカーソル

---
+++ Assets/Scripts/UIController_Overlay.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+public class UIController_Overlay : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform targetTfm;
+
+    private RectTransform myRectTfm;
+    [SerializeField] private Vector3 offset = new Vector3(0, 1.5f, 0);
+
+    void Start()
+    {
+        myRectTfm = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        myRectTfm.position
+            = RectTransformUtility.WorldToScreenPoint(Camera.main, targetTfm.position + offset);
+    }
+}
