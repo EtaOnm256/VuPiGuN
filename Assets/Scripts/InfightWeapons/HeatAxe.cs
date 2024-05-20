090856a979e93dac4d8fcfb5da22a5cb7f75fd3f
EtaOnm256 <manabu4836@hotmail.com>
2024-05-20 22:06:34 +0900

    ヒートアックスモーション直し 見た目はそこそこになったけど通常格闘2段目がスカる

--- Assets/Scripts/InfightWeapons/HeatAxe.cs
+++ Assets/Scripts/InfightWeapons/HeatAxe.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class HeatAxe : InfightWeapon
 {
+    override public bool slashing
+    {
+        set
+        {
+            _slashing = value;
+
+            if (_slashing)
+                _emitting = true;
+
+            if (!_slashing)
+            {
+                hitHistoryCount = 0;
+                hitHistoryRCCount = 0;
+
+                System.Array.Fill(hitHistory, null);
+                System.Array.Fill(hitHistoryRC, null);
+            }
+
+        }
+        get { return _slashing; }
+    }
+
     override public bool emitting
     {
         set
@@ -69,5 +91,77 @@ public class HeatAxe : InfightWeapon
     void Update()
     {
 
+    }
+
+    const int num_points = 1;
+
+    Vector3[] prev_points = new Vector3[num_points];
+    Vector3[] points = new Vector3[num_points];
+
+    public GameObject hitpoint;
+
+    RaycastHit[] rayCastHit = new RaycastHit[8];
+
+    public GameObject hitEffect_prefab;
+
+    GameObject[] hitHistory = new GameObject[16];
+    RobotController[] hitHistoryRC = new RobotController[16];
+    int hitHistoryCount = 0;
+    int hitHistoryRCCount = 0;
+
+    void FixedUpdate()
+    {
+
+        for (int i = 0; i < num_points; i++)
+        {
+            points[i] = hitpoint.transform.position;
+        }
+
+
+
+        if (slashing)
+        {
+            for (int idx_point = 0; idx_point < num_points; idx_point++)
+            {
+                EvalHit(points[idx_point], prev_points[idx_point]);
+            }
+        }
+
+        points.CopyTo(prev_points, 0);
+    }
+
+    private void EvalHit(Vector3 p1, Vector3 p2)
+    {
+        Ray ray = new Ray(p1, p2 - p1);
+
+        float length = (p2 - p1).magnitude;
+
+        int numhit = Physics.RaycastNonAlloc(ray, rayCastHit, length, 1 << 6);
+
+        for (int idx_hit = 0; idx_hit < numhit; idx_hit++)
+        {
+            if (hitHistory.Contains(rayCastHit[idx_hit].collider.gameObject))
+                continue;
+
+            hitHistory[hitHistoryCount++] = rayCastHit[idx_hit].collider.gameObject;
+
+
+
+            RobotController robotController = rayCastHit[idx_hit].collider.gameObject.GetComponentInParent<RobotController>();
+
+            if (robotController != null)
+            {
+                if (hitHistoryRC.Contains(robotController))
+                    continue;
+
+                hitHistoryRC[hitHistoryRCCount++] = robotController;
+
+                robotController.DoDamage(dir, damage, strong);
+
+                GameObject.Instantiate(hitEffect_prefab, rayCastHit[idx_hit].point, Quaternion.identity);
+            }
+
+
+        }
     }
 }
