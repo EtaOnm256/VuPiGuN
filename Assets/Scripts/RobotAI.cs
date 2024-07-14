2e99e4708b349787fb654f4446073de6324e1374
EtaOnm256 <manabu4836@hotmail.com>
2024-07-14 19:12:08 +0900

    起伏を検知してジャンプで乗り越えるようにした（迂回した方がいいビルとかは別扱い予定

--- Assets/Scripts/RobotAI.cs
+++ Assets/Scripts/RobotAI.cs
@@ -22,8 +22,12 @@ public class RobotAI : InputBase
     bool boosting = false;
     bool ascending = false;
 
+    public int ascend_margin = 0;
+
     public int fire_wait;
     public int fire_prepare = 15;
+
+    bool overheating = false;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -39,7 +43,7 @@ public class RobotAI : InputBase
 
             foreach (var robot in team.robotControllers)
             {
-                float dist = (robotController.GetCenter() - robot.GetCenter()).sqrMagnitude;
+                float dist = (robotController.GetCenter() - robot.GetCenter()).magnitude;
 
                 if (dist < mindist)
                 {
@@ -56,111 +60,179 @@ public class RobotAI : InputBase
         }
         else
         {
-            //robotController.cameraRotation = robotController.GetTargetQuaternionForView(nearest_robot);
 
             Vector3 cameraAxis = robotController.GetTargetQuaternionForView(nearest_robot).eulerAngles;
 
             robotController._cinemachineTargetYaw = cameraAxis.y;
             robotController._cinemachineTargetPitch = cameraAxis.x;
 
-            
+            Quaternion targetQ = Quaternion.LookRotation(nearest_robot.GetCenter() - robotController.GetCenter(), Vector3.up);
+
+            /*Vector3 move_dir = robotController.cameraRotation * Vector3.forward;
+
+            move_dir.y = 0.0f;
+            move_dir = move_dir.normalized;*/
+
             fire = false;
             sprint = false;
+
+            if (overheating)
+            {
+                if (robotController.boost >= robotController.Boost_Max)
+                    overheating = false;
+            }
+            else
+            {
+                if (robotController.boost <= 0)
+                    overheating = true;
+            }
+
             if (robotController.upperBodyState == RobotController.UpperBodyState.FIRE)
             {
                 move = Vector2.zero;
             }
             else
             {
-                move = Vector2.zero;
-                /*if (fire_wait <= 0)
-                {
-                    if (System.Math.Sqrt(mindist) < 100.0f)
-                    {
-                        if (fire_prepare <= 0)
-                        {
-                            fire = true;
-                            fire_wait = Random.Range(60, 120);
-                            fire_prepare = 15;
-                        }
-                        else
-                        {
-                            fire_prepare--;
-                        }
+                RaycastHit hit;
+                bool Aim_blocked = Physics.Raycast(robotController.GetCenter(), targetQ * Vector3.forward, out hit, mindist, 1 << 3);
 
-                        move = Vector2.zero;
-                        moveDirChangeTimer = 0;
+                if(ascending)
+                {
+                    if (!Aim_blocked)
+                        ascend_margin--;
+                    else
+                        ascend_margin = 60;
+
+                    if (ascend_margin <= 0)
+                    {
+                        ascending = false;
+                    }
+                    
+                }
+                else
+                {
+                    if (Aim_blocked)
+                    {
+                        if (hit.distance < 10.0f)
+                        {
+                            ascending = true;
+                            ascend_margin = 60;
+                        }
+                    }
+
+                }
+
+                if (Aim_blocked)
+                {
+                    move.y = 1.0f;
+                    move.x = 0.0f;
+
+                    if (overheating || !ascending)
+                    {
+                        jump = false;
                     }
                     else
                     {
-                        fire_wait = Random.Range(60, 120);
-                        fire_prepare = 15;
+                        jump = true;
                     }
                 }
-                else*/
+                else
                 {
-                    bool dodge = false;
+                    jump = false;
 
-                    foreach (var team in robotController.worldManager.teams)
+                    if (fire_wait <= 0)
                     {
-                        if (team == robotController.team)
-                            continue;
-
-                        foreach (var projectile in team.projectiles)
+                        if (mindist < 100.0f)
                         {
-                            if (projectile.dead)
-                                continue;
-
-                            //if ( Vector3.Dot(.normalized,projectile.direction.normalized) > Mathf.Cos(Mathf.PI/4))
-
-                            float shift = Vector3.Cross(projectile.direction.normalized, (robotController.GetCenter() - projectile.position)).magnitude;
-
-                            float dist = (robotController.GetCenter() - projectile.position).magnitude;
-
-                            if (Vector3.Dot((robotController.GetCenter() - projectile.transform.position).normalized, projectile.direction.normalized) > Mathf.Cos(Mathf.PI / 4)
-                                && (/*projectile.target == robotController || */shift < 3.0f)
-                                && dist/projectile.speed < 20.0f
-                                )
+                            if (fire_prepare <= 0)
                             {
-                                dodge = true;
-                                break;
+                                fire = true;
+                                fire_wait = Random.Range(60, 120);
+                                fire_prepare = 15;
                             }
-                        }
-                    }
+                            else
+                            {
+                                fire_prepare--;
+                            }
 
-                    if (dodge)
-                    {
-                        move.x = 1.0f;
-                        sprint = true;
-                        moveDirChangeTimer = 60;
-                    }
-                    /*else
-                    {
-                        if (System.Math.Sqrt(mindist) > 75.0f)
-                        {
-                            move.y = 1.0f;
-                            //moveDirChangeTimer = 60;
+                            move = Vector2.zero;
+                            moveDirChangeTimer = 0;
                         }
                         else
                         {
+                            fire_wait = Random.Range(60, 120);
+                            fire_prepare = 15;
+                        }
+                    }
+                    else
+                    {
 
+                        bool dodge = false;
 
-                            if (moveDirChangeTimer == 0)
+                        foreach (var team in robotController.worldManager.teams)
+                        {
+                            if (team == robotController.team)
+                                continue;
+
+                            foreach (var projectile in team.projectiles)
                             {
-                                move = VectorUtil.rotate(new Vector2(1.0f, 0.0f), Random.Range(0, 360.0f));
-                                moveDirChangeTimer = 60;
+                                if (projectile.dead)
+                                    continue;
+
+                                //if ( Vector3.Dot(.normalized,projectile.direction.normalized) > Mathf.Cos(Mathf.PI/4))
+
+                                float shift = Vector3.Cross(projectile.direction.normalized, (robotController.GetCenter() - projectile.position)).magnitude;
+
+                                float dist = (robotController.GetCenter() - projectile.position).magnitude;
+
+                                if (Vector3.Dot((robotController.GetCenter() - projectile.transform.position).normalized, projectile.direction.normalized) > Mathf.Cos(Mathf.PI / 4)
+                                    && (/*projectile.target == robotController || */shift < 3.0f)
+                                    && dist / projectile.speed < 20.0f
+                                    )
+                                {
+                                    dodge = true;
+                                    break;
+                                }
                             }
                         }
-                    }*/
-                    fire_wait--;
-                    //moveDirChangeTimer--;
+
+                        if (dodge)
+                        {
+                            move.x = 1.0f;
+                            move.y = 0.0f;
+                            sprint = true;
+                            moveDirChangeTimer = 60;
+                        }
+                        else
+                        {
+                            if (mindist > 75.0f)
+                            {
+                                move.y = 1.0f;
+                                move.x = 0.0f;
+                                //moveDirChangeTimer = 60;
+                            }
+                            else
+                            {
+
+
+                                if (moveDirChangeTimer <= 0)
+                                {
+                                    move = VectorUtil.rotate(new Vector2(1.0f, 0.0f), Random.Range(0, 360.0f));
+                                    moveDirChangeTimer = 60;
+                                }
+                            }
+                        }
+
+                        fire_wait--;
+                        moveDirChangeTimer--;
+                    }
                 }
             }
         }
 
 
         return;
-        fire = true;
+      /*  fire = true;
         //slash = true;
         subfire = false;
         if (boosting)
@@ -224,6 +296,6 @@ public class RobotAI : InputBase
         moveDirChangeTimer--;
 
         if (robotController.Target_Robot != null)
-            robotController.cameraRotation = robotController.GetTargetQuaternionForView(robotController.Target_Robot);
+            robotController.cameraRotation = robotController.GetTargetQuaternionForView(robotController.Target_Robot);*/
     }
 }
